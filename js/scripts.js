9f45c02d3aab8dfd006c4a169745ffbe3e1df005
Julia Joseph <juljoseph33@hotmail.com>
2015-06-17 22:13:41 -0400

    finals

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+	$('#clickjj').click(function() {
+		$('#jj').show();
+		$('#is').hide();
+		$('#ean').hide();
+	});
+	$('#clickis').click(function() {
+		$('#is').show();
+		$('#jj').hide();
+		$('#ean').hide();
+	});
+	$('#clickean').click(function() {
+		$('#ean').show();
+		$('#is').hide();
+		$('#jj').hide();
+	});
 	$('#nav').scrollToFixed();
 	$(window).stellar({
 		hideDistantElements: true,
