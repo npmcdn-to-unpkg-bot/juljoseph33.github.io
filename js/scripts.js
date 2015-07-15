c83c65f2e6f138688d2c62106db67fe205d7de8c
Julia Joseph <juljoseph33@hotmail.com>
2015-07-14 22:42:26 -0400

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,47 @@
 $(document).ready(function() {
+	// smooth scrolling
+	$(document).on('click', 'a[href^="#"]', function(e) {
+		var id = $(this).attr('href');
+		var $id = $(id);
+		if ($id.length === 0) {
+		    return;
+		}
+		e.preventDefault();
+		var pos = $(id).offset().top;
+		$('body, html').animate({scrollTop: pos});
+	});
+	$(document).on('click', 'button', function(e) {
+		var id = $(location).attr('href');
+		var $id = $(id);
+		if ($id.length === 0) {
+		    return;
+		}
+		e.preventDefault();
+		var pos = $(id).offset().top;
+		$('body, html').animate({scrollTop: pos});
+	});
+	// animate and toggle job functions
 	$('#clickjj').click(function() {
-		$('#jj').show();
-		$('#is').hide();
-		$('#ean').hide();
+		$('#jj').show("slow");
+		$('#is').hide("slow");
+		$('#ean').hide("slow");
 	});
 	$('#clickis').click(function() {
-		$('#is').show();
-		$('#jj').hide();
-		$('#ean').hide();
+		$('#is').show("slow");
+		$('#jj').hide("slow");
+		$('#ean').hide("slow");
 	});
 	$('#clickean').click(function() {
-		$('#ean').show();
-		$('#is').hide();
-		$('#jj').hide();
+		$('#ean').show("slow");
+		$('#is').hide("slow");
+		$('#jj').hide("slow");
 	});
+	// plugin for sticky header
 	$('#nav').scrollToFixed();
+	// parallax
 	$(window).stellar({
 		hideDistantElements: true,
 		verticalScrolling: true,
 		scrollProperty: 'position'
 	});
-	// $.smoothScroll();
 });
