911e3ef54249a231050aef5c7fb12213dcbf5344
Julia Joseph <juljoseph33@hotmail.com>
2015-07-15 22:11:42 -0400

    added portfolio support

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
+	var minuspixels = "";
+	var addpixels = "";
+	$(".show_hide").click(function () {
+		var num = $(this).attr('id');
+		$('.'+num).toggle();
+		if ($('.'+num).is(':visible')) {
+			$('#'+num).text(' Read Less');
+			var selector = '#'+$(this).parent('p').parent().attr('id') + ' p';
+			var pixels = getExtra(selector).toString()
+			var addpixels = '+='+pixels+'px';
+			minuspixels = '-='+pixels+'px';
+			$(this).parent().parent().css('margin-bottom', addpixels);
+		}
+		else {
+			$('#'+num).text(' ...Read More');
+			$(this).parent().parent().css('margin-bottom', minuspixels);
+		}
+	});
+	$(".fancybox").fancybox();
 	// smooth scrolling
 	$(document).on('click', 'a[href^="#"]', function(e) {
 		var id = $(this).attr('href');
-		var $id = $(id);
-		if ($id.length === 0) {
-		    return;
-		}
-		e.preventDefault();
-		var pos = $(id).offset().top;
-		$('body, html').animate({scrollTop: pos});
-	});
-	$(document).on('click', 'button', function(e) {
-		var id = $(location).attr('href');
 		var $id = $(id);
 		if ($id.length === 0) {
 		    return;
@@ -38,6 +47,8 @@ $(document).ready(function() {
 	});
 	// plugin for sticky header
 	$('#nav').scrollToFixed();
+
+	// FINAL LINES OF FUNCTION
 	// parallax
 	$(window).stellar({
 		hideDistantElements: true,
@@ -45,3 +56,11 @@ $(document).ready(function() {
 		scrollProperty: 'position'
 	});
 });
+function getExtra(selector) {
+    var height = $(selector).height();
+    var font_size = $(selector).css('font-size');
+    var scale = 1.15;
+    var before_height = Math.floor(parseInt(font_size) * scale * 3);
+    var rows = height - before_height;
+    return Math.round(rows);
+}
